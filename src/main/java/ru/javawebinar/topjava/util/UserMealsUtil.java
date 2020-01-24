ea9ceaab0b40639dd9ca3d25c5b350bb26e7facc
RabKvanta <37476810+RabKvanta@users.noreply.github.com>
2020-01-24 15:07:10 +0200

    HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -6,6 +6,7 @@ import ru.javawebinar.topjava.model.UserMealWithExcess;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,14 +24,22 @@ public class UserMealsUtil {
 
         List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsTo.forEach(System.out::println);
-
-//        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+     //   System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with excess. Implement by cycles
-        System.out.println("TODO return filtered list with excess. Implement by cycles");
-        return null;
+        List<UserMealWithExcess> list = new ArrayList<>();
+        for ( UserMeal meal : meals ){
+            LocalDateTime localDateTime = meal.getDateTime();
+            LocalTime localTime = localDateTime.toLocalTime();
+
+            if ( localTime.isAfter(startTime) && localTime.isBefore(endTime) ){
+                boolean excess = meal.getCalories() > caloriesPerDay;
+                list.add(new UserMealWithExcess(localDateTime, meal.getDescription(),meal.getCalories(),excess ));
+            }
+        }
+        return list;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
